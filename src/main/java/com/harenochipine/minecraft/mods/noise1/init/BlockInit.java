6a7b97aa1d363d3b681ec6884167772fc19a3a71
Noisepassfilter <noisepassfilter@gmail.com>
2020-12-26 20:15:39 +0900

    Added Ruby Ore block and made Ruby Sword a sword item

---
+++ src/main/java/com/harenochipine/minecraft/mods/noise1/init/BlockInit.java
@@ -0,0 +1,30 @@
+package com.harenochipine.minecraft.mods.noise1.init;
+
+import com.harenochipine.minecraft.mods.noise1.NoiseOne;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.registries.ObjectHolder;
+
+@ObjectHolder(NoiseOne.MOD_ID)
+@Mod.EventBusSubscriber(modid = NoiseOne.MOD_ID, bus = Bus.MOD)
+public class BlockInit {
+	public static final Block ruby_ore = null;
+
+	@SubscribeEvent
+	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
+		event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).setRequiresTool().hardnessAndResistance(3.0F, 3.0F)).setRegistryName("ruby_ore"));
+	}
+
+	@SubscribeEvent
+	public static void registerBlocksItems(final RegistryEvent.Register<Item> event) {
+		event.getRegistry().register(new BlockItem(ruby_ore, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName("ruby_ore"));
+	}
+}
--- src/main/java/com/harenochipine/minecraft/mods/noise1/init/ItemInit.java
+++ src/main/java/com/harenochipine/minecraft/mods/noise1/init/ItemInit.java
@@ -4,14 +4,16 @@ import com.harenochipine.minecraft.mods.noise1.NoiseOne;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.registries.ObjectHolder;
 
-@Mod.EventBusSubscriber(modid = NoiseOne.MOD_ID, bus = Bus.MOD)
 @ObjectHolder(NoiseOne.MOD_ID)
+@Mod.EventBusSubscriber(modid = NoiseOne.MOD_ID, bus = Bus.MOD)
 public class ItemInit {
 	public static final Item ruby = null;
 	public static final Item ruby_sword = null;
@@ -19,6 +21,7 @@ public class ItemInit {
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName("ruby"));
-		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName("ruby_sword"));
+		//event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName("ruby_sword"));
+		event.getRegistry().register(new SwordItem(ItemTier.DIAMOND, 15, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT)).setRegistryName("ruby_sword"));
 	}
 }
