ab2b8225575a4ae593448077689c22078a818ab1
Noisepassfilter <noisepassfilter@gmail.com>
2020-12-29 10:21:53 +0900

    Added new item group and made Ruby an edible item (including change for deplrecated effect in 1.16)

--- src/main/java/com/harenochipine/minecraft/mods/noise1/NoiseOne.java
+++ src/main/java/com/harenochipine/minecraft/mods/noise1/NoiseOne.java
@@ -1,9 +1,19 @@
 package com.harenochipine.minecraft.mods.noise1;
 
+import java.util.stream.Collectors;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.harenochipine.minecraft.mods.noise1.init.BlockInit;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
@@ -13,10 +23,6 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod("noise1")
@@ -25,19 +31,23 @@ public class NoiseOne
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
     public static final String MOD_ID = "noise1";
+    public static NoiseOne noiseOne;
 
     public NoiseOne() {
+    	noiseOne = this;
+
+    	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    	modEventBus.addListener(noiseOne::setup);
         // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+    	modEventBus.addListener(noiseOne::enqueueIMC);
         // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+    	modEventBus.addListener(noiseOne::processIMC);
         // Register the doClientStuff method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+    	modEventBus.addListener(noiseOne::doClientStuff);
 
         // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(noiseOne);
     }
 
     private void setup(final FMLCommonSetupEvent event)
@@ -81,5 +91,18 @@ public class NoiseOne
             // register a new block here
             LOGGER.info("HELLO from Register Block");
         }
+    }
+
+    public static class NoiseOneItemGroup extends ItemGroup {
+
+    	public static final NoiseOneItemGroup instance = new NoiseOneItemGroup(ItemGroup.GROUPS.length, "noise1");
+    	private NoiseOneItemGroup(int index, String label) {
+    		super(index, label);
+    	}
+
+    	@Override
+    	public ItemStack createIcon() {
+    		return new ItemStack(BlockInit.ruby_ore);
+    	}
     }
 }
--- src/main/java/com/harenochipine/minecraft/mods/noise1/init/ItemInit.java
+++ src/main/java/com/harenochipine/minecraft/mods/noise1/init/ItemInit.java
@@ -1,11 +1,15 @@
 package com.harenochipine.minecraft.mods.noise1.init;
 
 import com.harenochipine.minecraft.mods.noise1.NoiseOne;
+import com.harenochipine.minecraft.mods.noise1.NoiseOne.NoiseOneItemGroup;
 
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemTier;
 import net.minecraft.item.SwordItem;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,8 +24,10 @@ public class ItemInit {
 
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
-		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName("ruby"));
+		//event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MATERIALS)).setRegistryName("ruby"));
+		event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.MATERIALS).food(new Food.Builder().hunger(6).saturation(1.2f).effect(() -> new EffectInstance(Effects.HASTE, 6000, 5), 1).build())).setRegistryName("ruby"));
 		//event.getRegistry().register(new Item(new Item.Properties().group(ItemGroup.COMBAT)).setRegistryName("ruby_sword"));
-		event.getRegistry().register(new SwordItem(ItemTier.DIAMOND, 15, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT)).setRegistryName("ruby_sword"));
+		//event.getRegistry().register(new SwordItem(ItemTier.DIAMOND, 15, -2.4F, (new Item.Properties()).group(ItemGroup.COMBAT)).setRegistryName("ruby_sword"));
+		event.getRegistry().register(new SwordItem(ItemTier.DIAMOND, 15, -2.4F, (new Item.Properties()).group(NoiseOneItemGroup.instance)).setRegistryName("ruby_sword"));
 	}
 }
